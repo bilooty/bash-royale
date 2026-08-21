750c23c548727991053fb556bd276e5e394de484
bilooty <bill.j.rule@gmail.com>
2026-08-21 19:54:53 +1000

    UnitType setup

--- bash-royale/UnitType.cs
+++ bash-royale/UnitType.cs
@@ -7,7 +7,14 @@ public enum UnitType
 }
 public record UnitInfo(string Name, int cost);
 
-public static class UnitTypes
+public static class UnitInfos
 {
-    
+    public static UnitInfo GetUnitInfo(UnitType unitType)
+    {
+        return unitType switch
+        {
+            UnitType.Knight => new UnitInfo("Knight", 5),
+            UnitType.Giant => new UnitInfo("Giant", 6)
+        };
+    }
 }
