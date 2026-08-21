4fe2a3197163d9c9e3ff185ff5c0d46249f5641c
JinxsFishbonez <59107043+JinxsFishbonez@users.noreply.github.com>
2026-08-21 20:51:45 +1000

    add unit and card types

--- bash-royale/UnitType.cs
+++ bash-royale/UnitType.cs
@@ -3,9 +3,13 @@ namespace bash_royale;
 public enum UnitType
 {
     Knight,
-    Giant
+    Giant,
+    Archer,
+    Goblin,
+    Wizard,
+    Horde
 }
-public record UnitInfo(string Name, int cost);
+public record UnitInfo(string Name, int hp, bool flying, string attackType, bool siege, int damage);
 
 public static class UnitInfos
 {
@@ -13,8 +17,12 @@ public static class UnitInfos
     {
         return unitType switch
         {
-            UnitType.Knight => new UnitInfo("Knight", 5),
-            UnitType.Giant => new UnitInfo("Giant", 6)
+            UnitType.Knight => new UnitInfo("Knight", 10, false, "Melee", false, 2),
+            UnitType.Giant => new UnitInfo("Giant", 40, false, "Melee", true, 5),
+            UnitType.Archer => new UnitInfo("Archer", 4, false, "Ranged", false,2),
+            UnitType.Goblin => new UnitInfo("Goblin", 3, false, "Melee", false, 1),
+            UnitType.Wizard => new UnitInfo("Wizard", 8, false, "Ranged", false, 4),
+            UnitType.Horde => new UnitInfo("Horde", 20, true, "Melee", false, 2)
         };
     }
 }
